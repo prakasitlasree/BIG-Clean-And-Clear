fa8474d4ea4fced30fd661eae464d69ef6d1bbe3
Rock <Rock@DESKTOP-174O822>
2020-01-14 21:06:55 +0700

    update

--- BIG Clean and Care/BIG.Clean.Care.PRESENT/Controllers/NewsController.cs
+++ BIG Clean and Care/BIG.Clean.Care.PRESENT/Controllers/NewsController.cs
@@ -55,49 +55,57 @@ namespace BIG.Clean.Care.PRESENT.Controllers
             var header = Request.Form["SECTION_NAME"].ToString();
             var subHeader = Request.Form["HTML_SUB_HEADER1"].ToString();
             var content = Request.Form["HTML_VALUE"].ToString();
-
-
             var pathFolder = Server.MapPath("~/assets/images/news");
-            var pathfile = "";
-            var fileName = "";
-            for (int i = 0; i < files.Count; i++)
+            try
             {
-                HttpPostedFileBase file = files[i];
-                pathfile = Path.Combine(pathFolder, file.FileName);
-                fileName = file.FileName;
-                file.SaveAs(pathfile);
-
-            }
-
-            NewsService service = new NewsService();
-
-            if (Session["User"] != null)
-            {
-                var obj = (LOGON)Session["User"];
-                PAGE_CONTENT source = new PAGE_CONTENT()
+                //var pathFolder = Server.MapPath("~/assets/images/news");
+                var pathfile = "";
+                var fileName = "";
+                for (int i = 0; i < files.Count; i++)
                 {
-                    MODULE = "News",
-                    SECTION_NAME = header,
-                    HTML_SUB_HEADER1 = subHeader,
-                    HTML_VALUE = content,
-                    STATUS = 1,
-                    IMAGE_URL1 = pathfile,
-                    IMAGE_URL2 = fileName,
-                    CREATED_DATE = DateTime.Now,
-                    UPDATED_DATE = DateTime.Now,
-                    CREATED_BY = obj.USERNAME,
-                    UPDATED_BY = obj.USERNAME
-                };
+                    HttpPostedFileBase file = files[i];
+                    pathfile = Path.Combine(pathFolder, file.FileName);
+                    fileName = file.FileName;
+                    file.SaveAs(pathfile);
 
-                resp = service.AddNews(source);
+                }
+
+                NewsService service = new NewsService();
+
+                if (Session["User"] != null)
+                {
+                    var obj = (LOGON)Session["User"];
+                    PAGE_CONTENT source = new PAGE_CONTENT()
+                    {
+                        MODULE = "News",
+                        SECTION_NAME = header,
+                        HTML_SUB_HEADER1 = subHeader,
+                        HTML_VALUE = content,
+                        STATUS = 1,
+                        IMAGE_URL1 = pathfile,
+                        IMAGE_URL2 = fileName,
+                        CREATED_DATE = DateTime.Now,
+                        UPDATED_DATE = DateTime.Now,
+                        CREATED_BY = obj.USERNAME,
+                        UPDATED_BY = obj.USERNAME
+                    };
+
+                    resp = service.AddNews(source);
+                }
+                else
+                {
+                    resp.STATUS = false;
+                    resp.ERROR_MESSAGE = "กรุณาเข้าสู่ระบบใหม่";
+                }
             }
-            else
+            catch(Exception ex)
             {
                 resp.STATUS = false;
-                resp.ERROR_MESSAGE = "กรุณาเข้าสู่ระบบใหม่";
+                resp.MESSAGE = pathFolder;
+                resp.ERROR_MESSAGE = ex.Message;
             }
 
-
+          
             return Json(resp, JsonRequestBehavior.AllowGet);
         }
 
@@ -111,57 +119,66 @@ namespace BIG.Clean.Care.PRESENT.Controllers
             var header = Request.Form["SECTION_NAME"].ToString();
             var subHeader = Request.Form["HTML_SUB_HEADER1"].ToString();
             var content = Request.Form["HTML_VALUE"].ToString();
-
-
             var pathFolder = Server.MapPath("~/assets/images/news");
-            var pathfile = "";
-            var fileName = "";
-
-            if (files.Count > 0)
+            try
             {
-                for (int i = 0; i < files.Count; i++)
+                
+                var pathfile = "";
+                var fileName = "";
+
+                if (files.Count > 0)
                 {
-                    HttpPostedFileBase file = files[i];
-                    pathfile = Path.Combine(pathFolder, file.FileName);
-                    fileName = file.FileName;
-                    file.SaveAs(pathfile);
+                    for (int i = 0; i < files.Count; i++)
+                    {
+                        HttpPostedFileBase file = files[i];
+                        pathfile = Path.Combine(pathFolder, file.FileName);
+                        fileName = file.FileName;
+                        file.SaveAs(pathfile);
 
-                }
-            }
-            
-
-            NewsService service = new NewsService();
-
-            if (Session["User"] != null)
-            {
-                var obj = (LOGON)Session["User"];
-                PAGE_CONTENT source = new PAGE_CONTENT()
-                {
-                    ID = Convert.ToInt32(id),
-                    MODULE = "News",
-                    SECTION_NAME = header,
-                    HTML_SUB_HEADER1 = subHeader,
-                    HTML_VALUE = content,
-                    STATUS = 1,                
-                    CREATED_DATE = DateTime.Now,
-                    UPDATED_DATE = DateTime.Now,
-                    CREATED_BY = obj.USERNAME,
-                    UPDATED_BY = obj.USERNAME
-                };
-
-                if(files.Count > 0)
-                {
-                    source.IMAGE_URL1 = pathfile;
-                    source.IMAGE_URL2 = fileName;
+                    }
                 }
 
-                resp = service.EditNews(source);
+
+                NewsService service = new NewsService();
+
+                if (Session["User"] != null)
+                {
+                    var obj = (LOGON)Session["User"];
+                    PAGE_CONTENT source = new PAGE_CONTENT()
+                    {
+                        ID = Convert.ToInt32(id),
+                        MODULE = "News",
+                        SECTION_NAME = header,
+                        HTML_SUB_HEADER1 = subHeader,
+                        HTML_VALUE = content,
+                        STATUS = 1,
+                        CREATED_DATE = DateTime.Now,
+                        UPDATED_DATE = DateTime.Now,
+                        CREATED_BY = obj.USERNAME,
+                        UPDATED_BY = obj.USERNAME
+                    };
+
+                    if (files.Count > 0)
+                    {
+                        source.IMAGE_URL1 = pathfile;
+                        source.IMAGE_URL2 = fileName;
+                    }
+
+                    resp = service.EditNews(source);
+                }
+                else
+                {
+                    resp.STATUS = false;
+                    resp.ERROR_MESSAGE = "กรุณาเข้าสู่ระบบใหม่";
+                }
             }
-            else
+            catch(Exception ex)
             {
                 resp.STATUS = false;
-                resp.ERROR_MESSAGE = "กรุณาเข้าสู่ระบบใหม่";
+                resp.MESSAGE = pathFolder;
+                resp.ERROR_MESSAGE = ex.Message;
             }
+           
 
 
             return Json(resp, JsonRequestBehavior.AllowGet);
